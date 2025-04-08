afd5059f5174ee9ccf0f68f5367323d5e9c0b5ea
Saverio Negro <saverionegro@gmail.com>
2025-04-08 13:02:28 +0200

    Initial Commit

---
+++ WallArt/WallArtApp.swift
@@ -0,0 +1,33 @@
+//
+//  WallArtApp.swift
+//  WallArt
+//
+//  Created by Saverio Negro on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WallArtApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
